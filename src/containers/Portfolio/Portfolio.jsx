f70b23d7e8d8ce49f84ac07f92814984cf0b0370
a.moruzhko <andrii@trujay.com>
2023-11-07 19:24:58 +0200

    styling

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 import Slider from "../../components/Slider/Slider";
+import ProjectCard from "../../containers/Portfolio/ProjectCard/ProjectCard";
+
 import { projects } from "../../constants/projects";
 
 import "./Portfolio.scss";
 
 const Portfolio = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section id="portfolio">
       <h2 className="head-text">My Recent Works</h2>
@@ -16,8 +32,15 @@ const Portfolio = () => {
           transition={{ duration: 0.5 }}
           className="portfolio__items"
         >
+          {screenWidth >= 1640 ? (
+            <Slider items={projects} itemsCount={2} />
+          ) : (
+            <>
+            {projects.map((project, index) => (
+            <ProjectCard className="active" key={index} project={project} />
+          ))}</>
+          )}
 
-          <Slider items={projects} itemsCount={2}/>
           {/* {projects.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))} */}
